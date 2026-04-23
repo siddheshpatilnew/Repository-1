e493206c7a9283162ca76503c87fd7d6e86d0cc8
siddheshpatilnew <siddhesh191919@gmail.com>
2026-04-23 14:00:28 +0530

    Add files via upload

---
+++ 9.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() 
+{
+    int num, temp, digit, sum = 0;
+
+    printf("Enter a three-digit number: ");
+    scanf("%d", &num);
+
+    temp = num;
+
+    while (temp > 0) 
+    {
+        digit = temp % 10;
+        sum = sum + (digit * digit * digit);
+        temp = temp / 10;
+    }
+
+    if (sum == num)
+        printf("%d is an Armstrong number.\n", num);
+    else
+        printf("%d is not an Armstrong number.\n", num);
+
+    return 0;
+}
