8e4f2dc45b8de334e0f4d11b340deeae10aeb8c6
siddheshpatilnew <siddhesh191919@gmail.com>
2026-04-23 13:58:19 +0530

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num, i;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Multiplication table of %d:\n", num);
+
+    // This loop runs from 1 to 10
+    for (i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
+
+    return 0;
+}
